2f431d2e8a3ef7218673fdeb368364c822556fe8
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-19 22:20:36 +0300

    feat: add ReferenceMapper

---
+++ src/main/java/hexlet/code/mapper/ReferenceMapper.java
@@ -0,0 +1,26 @@
+package hexlet.code.mapper;
+
+import hexlet.code.model.BaseEntity;
+import jakarta.persistence.EntityManager;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.TargetType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
+public abstract class ReferenceMapper {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
+        return id != null ? entityManager.find(entityClass, id) : null;
+    }
+
+}
