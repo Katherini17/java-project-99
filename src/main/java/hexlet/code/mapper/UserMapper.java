8a3400057d716e1d38fa380ba5b8695b51401fb2
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-21 16:22:15 +0300

    refactor: ignore password field in UserMapper

--- src/main/java/hexlet/code/mapper/UserMapper.java
+++ src/main/java/hexlet/code/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import hexlet.code.dto.UserDTO;
 import hexlet.code.dto.UserUpdateDTO;
 import hexlet.code.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
@@ -17,7 +18,12 @@ import org.mapstruct.ReportingPolicy;
     unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
+
+    @Mapping(target = "password", ignore = true)
     public abstract User map(UserCreateDTO dto);
+
     public abstract UserDTO map(User model);
+
+    @Mapping(target = "password", ignore = true)
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
 }
