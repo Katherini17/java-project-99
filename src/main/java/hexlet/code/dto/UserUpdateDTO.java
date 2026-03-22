eba4ae1d1e2a6ccd05ebd4348becc7f15d09f435
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-22 16:56:52 +0300

    refactor: add @NotBlank validation to UserUpdateDTO fields

--- src/main/java/hexlet/code/dto/UserUpdateDTO.java
+++ src/main/java/hexlet/code/dto/UserUpdateDTO.java
@@ -1,6 +1,7 @@
 package hexlet.code.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,9 +13,11 @@ public class UserUpdateDTO {
     private JsonNullable<String> firstName;
     private JsonNullable<String> lastName;
 
+    @NotBlank
     @Email
     private JsonNullable<String> email;
 
+    @NotBlank
     @Size(min = 3)
     private JsonNullable<String> password;
 
