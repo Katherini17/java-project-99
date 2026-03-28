5e11f095270184251677458ebc864df4b9378d83
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-28 19:18:32 +0300

    refactor: convert UserDTO and UserCreateDTO to records

--- src/main/java/hexlet/code/dto/UserDTO.java
+++ src/main/java/hexlet/code/dto/UserDTO.java
@@ -1,16 +1,11 @@
 package hexlet.code.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.Instant;
 
-@Getter
-@Setter
-public class UserDTO {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private Instant createdAt;
-}
+public record UserDTO(
+    Long id,
+    String firstName,
+    String lastName,
+    String email,
+    Instant createdAt
+) { }
