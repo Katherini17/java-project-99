808099b155b64bafa6694bd58c57a7dc407268f0
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-28 23:54:37 +0300

    feat: add TaskStatus entity with name and unique slug

---
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -0,0 +1,43 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "task_statuses")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class TaskStatus implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    @Size(min = 1)
+    private String name;
+
+    @NotBlank
+    @Column(unique = true)
+    @Size(min = 1)
+    private String slug;
+
+    @CreatedDate
+    private Instant createdAt;
+
+}
