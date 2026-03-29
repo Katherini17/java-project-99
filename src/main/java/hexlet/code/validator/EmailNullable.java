5ec18f422a13bc91abe0830d598ba85a7fc99f24
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-29 17:43:00 +0300

    feat: add custom EmailNullable validator for JsonNullable fields

---
+++ src/main/java/hexlet/code/validator/EmailNullable.java
@@ -0,0 +1,18 @@
+package hexlet.code.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EmailNullableValidator.class)
+@Target({ ElementType.FIELD }) // Вешаем на поля
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EmailNullable {
+    String message() default "Invalid email format";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
