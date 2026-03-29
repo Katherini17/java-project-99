1b96212435d8e41f1cb55570d6b91213dddb611f
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-03-29 19:27:30 +0300

    test: add TaskStatusGenerator with modular build methods

---
+++ src/test/java/hexlet/code/util/generator/TaskStatusGenerator.java
@@ -0,0 +1,45 @@
+package hexlet.code.util.generator;
+
+import hexlet.code.dto.taskstatus.TaskStatusCreateDTO;
+import hexlet.code.model.TaskStatus;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.instancio.Model;
+import org.instancio.Select;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@RequiredArgsConstructor
+public class TaskStatusGenerator {
+
+    private Model<TaskStatus> taskStatusModel;
+    private Model<TaskStatusCreateDTO> taskStatusCreateDTOModel;
+
+    private final Faker faker;
+
+    @PostConstruct
+    public void init() {
+        taskStatusModel = buildTaskStatusModel();
+        taskStatusCreateDTOModel = buildTaskStatusCreateDTOModel();
+    }
+
+    private Model<TaskStatus> buildTaskStatusModel() {
+        return Instancio.of(TaskStatus.class)
+                .ignore(Select.field(TaskStatus::getId))
+                .ignore(Select.field(TaskStatus::getCreatedAt))
+                .supply(Select.field(TaskStatus::getName), () -> faker.lorem().word())
+                .supply(Select.field(TaskStatus::getSlug), () -> faker.internet().slug())
+                .toModel();
+    }
+
+    private Model<TaskStatusCreateDTO> buildTaskStatusCreateDTOModel() {
+        return Instancio.of(TaskStatusCreateDTO.class)
+                .supply(Select.field(TaskStatusCreateDTO::name), () -> faker.lorem().word())
+                .supply(Select.field(TaskStatusCreateDTO::slug), () -> faker.internet().slug())
+                .toModel();
+    }
+}
