80b1ab563605996259ee42c1d6055ff59fb5162e
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-01 23:00:31 +0300

    feat(mapper): add TaskStatusConverter for slug-to-entity mapping

---
+++ src/main/java/hexlet/code/mapper/TaskStatusConverter.java
@@ -0,0 +1,25 @@
+package hexlet.code.mapper;
+
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.TaskStatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class TaskStatusConverter {
+
+    private final TaskStatusRepository taskStatusRepository;
+
+    public TaskStatus toEntity(String slug) {
+        return Optional.ofNullable(slug)
+                .flatMap(taskStatusRepository::findBySlug)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "TaskStatus with slug: %s not found".formatted(slug)
+                ));
+    }
+}
+
