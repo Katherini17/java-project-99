0887abd5a0bd0fbee2554b1146c194bbefb4ae2c
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-01 19:51:09 +0300

    test: add ModelUtils for JsonNullable testing

---
+++ src/test/java/hexlet/code/util/ModelUtils.java
@@ -0,0 +1,14 @@
+package hexlet.code.util;
+
+import org.instancio.Instancio;
+import org.instancio.Model;
+import org.instancio.Select;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+public class ModelUtils {
+    public static <T> Model<T> buildUpdateModel(Class<T> clazz) {
+        return Instancio.of(clazz)
+                .set(Select.all(JsonNullable.class), JsonNullable.undefined())
+                .toModel();
+    }
+}
