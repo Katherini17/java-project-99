3f50f5819e312b13e6a6c0f0846592ed26486a49
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-01 20:02:54 +0300

    refactor(dto): convert UserUpdateDTO and TaskStatusUpdateDTO to records

--- src/main/java/hexlet/code/dto/taskstatus/TaskStatusUpdateDTO.java
+++ src/main/java/hexlet/code/dto/taskstatus/TaskStatusUpdateDTO.java
@@ -1,18 +1,13 @@
 package hexlet.code.dto.taskstatus;
 
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
-
-@Getter
-@Setter
-public class TaskStatusUpdateDTO {
+public record TaskStatusUpdateDTO(
 
     @Size(min = 1)
-    private JsonNullable<String> name;
+    JsonNullable<String> name,
 
     @Size(min = 1)
-    private JsonNullable<String> slug;
+    JsonNullable<String> slug
 
-}
+) { }
--- src/main/java/hexlet/code/dto/user/UserUpdateDTO.java
+++ src/main/java/hexlet/code/dto/user/UserUpdateDTO.java
@@ -2,20 +2,15 @@ package hexlet.code.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
 
-@Getter
-@Setter
-public class UserUpdateDTO {
-    private JsonNullable<String> firstName;
-    private JsonNullable<String> lastName;
+public record UserUpdateDTO(
+    JsonNullable<String> firstName,
+    JsonNullable<String> lastName,
 
     @Email
-    private JsonNullable<String> email;
+    JsonNullable<String> email,
 
     @Size(min = 3)
-    private JsonNullable<String> password;
-
-}
+    JsonNullable<String> password
+) { }
