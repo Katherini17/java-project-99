0b437fa596709687f76d597a6d74063830c3fec6
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-02 19:13:04 +0300

    feat(controller): implement tasks managemen with pagination and security

---
+++ src/main/java/hexlet/code/controller/api/TasksController.java
@@ -0,0 +1,86 @@
+package hexlet.code.controller.api;
+
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.service.TaskService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static hexlet.code.util.PageUtils.buildPagingResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/tasks")
+@Validated
+@PreAuthorize("isAuthenticated()")
+public class TasksController {
+
+    private final TaskService taskService;
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<TaskDTO>> index(
+            @RequestParam(defaultValue = "1") @Min(1) int page,
+            @RequestParam(defaultValue = "10") @Min(1) @Max(100) int perPage,
+            @RequestParam(defaultValue = "id") String sort,
+            @RequestParam(defaultValue = "ASC") Sort.Direction order
+    ) {
+        var pageRequest = PageRequest.of(
+                page - 1,
+                perPage,
+                Sort.by(order, sort)
+        );
+        Page<TaskDTO> resultPage = taskService.getAll(pageRequest);
+
+        return buildPagingResponse(resultPage);
+    }
+
+    @GetMapping(path = "/{id}")
+    public TaskDTO show(@PathVariable Long id) {
+        return taskService.findById(id);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDTO create(@RequestBody @Valid TaskCreateDTO taskData) {
+        return taskService.create(taskData);
+    }
+
+    @PutMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @taskUtils.isAssignee(#id, authentication.name)")
+    public TaskDTO update(
+            @RequestBody @Valid TaskUpdateDTO taskData,
+            @PathVariable Long id
+    ) {
+        return taskService.update(taskData, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ADMIN') or @taskUtils.isAssignee(#id, authentication.name)")
+    public void destroy(@PathVariable Long id) {
+        taskService.delete(id);
+    }
+
+}
