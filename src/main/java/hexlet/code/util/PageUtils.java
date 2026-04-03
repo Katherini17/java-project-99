65fcc05e0d7f0ef5662ea6ebe43091f78e1227b1
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-03 23:21:49 +0300

    refactor(util): convert PageUtils to final utility class and fix code smells

--- src/main/java/hexlet/code/util/PageUtils.java
+++ src/main/java/hexlet/code/util/PageUtils.java
@@ -1,23 +1,29 @@
 package hexlet.code.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
-public class PageUtils {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PageUtils {
+
+    private static final String TOTAL_COUNT_HEADER = "X-Total-Count";
 
     public static <T> ResponseEntity<List<T>> buildPagingResponse(Page<T> page) {
+
         return ResponseEntity.ok()
-                .header("X-Total-Count", String.valueOf(page.getTotalElements()))
-                .header("Access-Control-Expose-Headers", "X-Total-Count")
+                .header(TOTAL_COUNT_HEADER, String.valueOf(page.getTotalElements()))
+                .header("Access-Control-Expose-Headers", TOTAL_COUNT_HEADER)
                 .body(page.getContent());
     }
 
     public static <T> ResponseEntity<List<T>> buildPagingResponse(List<T> content) {
         return ResponseEntity.ok()
-                .header("X-Total-Count", String.valueOf(content.size()))
-                .header("Access-Control-Expose-Headers", "X-Total-Count")
+                .header(TOTAL_COUNT_HEADER, String.valueOf(content.size()))
+                .header("Access-Control-Expose-Headers", TOTAL_COUNT_HEADER)
                 .body(content);
     }
 
