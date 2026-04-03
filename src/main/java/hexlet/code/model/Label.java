309de59712b5b67a76c123cf90c25975d3996d5c
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-03 23:55:50 +0300

    feat(model): add Label entity

---
+++ src/main/java/hexlet/code/model/Label.java
@@ -0,0 +1,38 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "labels")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Label {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @CreatedDate
+    private Instant createdAt;
+
+}
