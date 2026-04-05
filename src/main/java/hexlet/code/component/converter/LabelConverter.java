fd8c6d6d525334e8c8a54688987d3269355ff58e
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-05 20:22:17 +0300

    feat(converter): implement label set conversion

---
+++ src/main/java/hexlet/code/component/converter/LabelConverter.java
@@ -0,0 +1,41 @@
+package hexlet.code.component.converter;
+
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.exception.UnprocessableEntityException;
+import hexlet.code.model.Label;
+import hexlet.code.model.Task;
+import hexlet.code.repository.LabelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class LabelConverter {
+
+    private final LabelRepository labelRepository;
+
+    public Set<Label> toEntity(Set<Long> labelIds) {
+        if (labelIds == null) {
+            return null;
+        }
+
+        return Optional.of(labelIds)
+                .map(labelRepository::findAllById)
+                .map(HashSet::new)
+                .filter(labels -> labels.size() == labelIds.size())
+                .orElseThrow(() -> new UnprocessableEntityException("One or more labels not found"));
+    }
+
+    public Set<Long> toIds(Set<Label> labels) {
+        return Optional.ofNullable(labels)
+                .map(list -> list.stream()
+                        .map(Label::getId)
+                        .collect(Collectors.toSet()))
+                .orElse(null);
+    }
+}
