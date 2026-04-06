133daa95118cf614a5be990a65336704dba88813
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-06 22:32:18 +0300

    test(util): remove outdated test case for deleted List-based paging method

--- src/test/java/hexlet/code/util/PageUtilsTest.java
+++ src/test/java/hexlet/code/util/PageUtilsTest.java
@@ -1,28 +1,14 @@
 package hexlet.code.util;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.PageImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PageUtilsTest {
-
-    @Test
-    void testBuildPagingResponseFromList() {
-        var content = List.of("item1", "item2", "item3");
-
-        var response = PageUtils.buildPagingResponse(content);
-
-        assertThat(response).isNotNull().satisfies(res -> {
-            assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(res.getHeaders().getFirst("X-Total-Count")).isEqualTo("3");
-            assertThat(res.getHeaders().getFirst("Access-Control-Expose-Headers")).isEqualTo("X-Total-Count");
-            assertThat(res.getBody()).containsExactly("item1", "item2", "item3");
-        });
-    }
 
     @Test
     void testBuildPagingResponseFromPage() {
