3dad54fea9e24a8b3784ab8c4d3ccf72de478aa3
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-06 22:33:12 +0300

    refactor(repository): remove redundant public modifier in LabelRepository

--- src/main/java/hexlet/code/repository/LabelRepository.java
+++ src/main/java/hexlet/code/repository/LabelRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface LabelRepository extends JpaRepository<Label, Long> {
-    public Optional<Label> findByName(String name);
+    Optional<Label> findByName(String name);
 }
