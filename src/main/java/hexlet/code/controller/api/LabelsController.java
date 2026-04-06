db8fe2a76c3c54b06086d93d23f698cd2e4d049d
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-06 22:28:45 +0300

    feat(controller): implement LabelsController with full CRUD

---
+++ src/main/java/hexlet/code/controller/api/LabelsController.java
@@ -0,0 +1,71 @@
+package hexlet.code.controller.api;
+
+import hexlet.code.dto.label.LabelCreateDTO;
+import hexlet.code.dto.label.LabelDTO;
+import hexlet.code.dto.label.LabelUpdateDTO;
+import hexlet.code.service.LabelService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static hexlet.code.util.PageUtils.buildPagingResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/labels")
+@Validated
+@PreAuthorize("isAuthenticated()")
+@Tag(name = "Labels", description = "Dictionary of labels for grouping tasks")
+public class LabelsController {
+
+    private final LabelService labelService;
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<LabelDTO>> index() {
+        Page<LabelDTO> resultPage = labelService.getAll(Pageable.unpaged());
+        return buildPagingResponse(resultPage);
+    }
+
+    @GetMapping(path = "/{id}")
+    public LabelDTO show(@PathVariable Long id) {
+        return labelService.findById(id);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public LabelDTO create(@RequestBody @Valid LabelCreateDTO labelData) {
+        return labelService.create(labelData);
+    }
+
+    @PutMapping(path = "/{id}")
+    public LabelDTO update(
+            @RequestBody @Valid LabelUpdateDTO labelData,
+            @PathVariable Long id
+    ) {
+        return labelService.update(labelData, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        labelService.delete(id);
+    }
+
+}
