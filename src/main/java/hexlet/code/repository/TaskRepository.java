3d9ba9cc0ba2f933d5cbd7d8f679850d6c7bd207
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-07 21:37:29 +0300

    feat(repository): add JpaSpecificationExecutor to TaskRepository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package hexlet.code.repository;
 import hexlet.code.model.Task;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,10 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends
+        JpaRepository<Task, Long>,
+        JpaSpecificationExecutor<Task> {
+
     boolean existsByAssigneeId(Long userId);
     boolean existsByTaskStatusId(Long statusId);
     boolean existsByLabelsId(Long labelId);
