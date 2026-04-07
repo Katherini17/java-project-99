234def25c8156f915131a429e6246c2cf9c45877
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-07 04:23:46 +0300

    feat(repository): add pessimistic locking for data integrity

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,11 +1,23 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.Task;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     boolean existsByAssigneeId(Long userId);
     boolean existsByTaskStatusId(Long statusId);
+    boolean existsByLabelsId(Long labelId);
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT t FROM Task t WHERE t.id = :id")
+    Optional<Task> findWithLockById(@Param("id") Long id);
 }
--- src/main/java/hexlet/code/repository/TaskStatusRepository.java
+++ src/main/java/hexlet/code/repository/TaskStatusRepository.java
@@ -1,7 +1,11 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.TaskStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +13,8 @@ import java.util.Optional;
 @Repository
 public interface TaskStatusRepository extends JpaRepository<TaskStatus, Long> {
     Optional<TaskStatus> findBySlug(String slug);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT ts FROM TaskStatus ts WHERE ts.id = :id")
+    Optional<TaskStatus> findWithLockById(@Param("id") Long id);
 }
