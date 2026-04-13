7125fdac31b17d3915083004afdac4ade80a0965
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-13 20:57:45 +0300

    docs(controller): add swagger documentation for WelcomeController

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,10 +1,15 @@
 package hexlet.code.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Welcome", description = "Root and health check endpoints")
 public class WelcomeController {
+
+    @Operation(summary = "Get welcome message")
     @GetMapping(path = "/welcome")
     public String root() {
         return "Welcome to Spring";
