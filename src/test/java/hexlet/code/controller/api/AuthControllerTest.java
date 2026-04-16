7ee7b35c0e9b50c509cd3d2eb3fc3b776da728e4
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-04-16 22:33:22 +0300

    refactor(api): remove redundant @Transactional from AuthControllerTest

--- src/test/java/hexlet/code/controller/api/AuthControllerTest.java
+++ src/test/java/hexlet/code/controller/api/AuthControllerTest.java
@@ -10,13 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
